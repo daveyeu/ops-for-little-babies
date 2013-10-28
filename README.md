df906cfc1160578f40181b751919a387410b094e
Dave Yeu <daveyeu@gmail.com>
2013-10-28 15:01:43 -0400

    Initial commit
    "Forked" from the original talk, "Operations at Groupme":
    
    http://daveyeu.com/ops-at-groupme
    https://github.com/daveyeu/ops-at-groupme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Operations at GroupMe #
+
+A presentation given at hackNY Masters.
+
+This HTML slideshow is modified from the [Google I/O slides][1].
+
+
+[1]: https://code.google.com/p/io-2012-slides/
---
+++ slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title:    'Operations at GroupMe',
+    subtitle: '(╯°□°)╯︵ ┻━┻',
+    eventInfo: {
+     title: 'hackNY Masters',
+     date:  '9/28/2013'
+    },
+    useBuilds:        true, // Default: true. False will turn off slide animation builds.
+    usePrettify:      true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch:      true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/poundie_24.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['groupme'],
+  },
+
+  // Author information
+  presenters: [{
+    name:    'Dave Yeu',
+    twitter: '@daveyeu',
+    github:  'https://github.com/daveyeu'
+  }]
+};
